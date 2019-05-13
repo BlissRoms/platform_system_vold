6d954e4a1e95586bf0c5cd1f3080c8995a50b601
Jackeagle <jackeagle102@gmail.com>
2019-05-13 06:00:24 +0200

    cryptfs: Add ifndef guard to fix following error
    system/vold/cryptfs.cpp:1270:12: error: unused function 'add_sector_size_param' [-Werror,-Wunused-function]
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Ic5614143f3312cbb0ccc498606f2900ff5d3aeb2

--- cryptfs.cpp
+++ cryptfs.cpp
@@ -1262,6 +1262,7 @@ static std::string extra_params_as_string(const std::vector<std::string>& extra_
 }
 #endif
 
+#ifndef CONFIG_HW_DISK_ENCRYPTION
 /*
  * If the ro.crypto.fde_sector_size system property is set, append the
  * parameters to make dm-crypt use the specified crypto sector size and round
@@ -1294,6 +1295,7 @@ static int add_sector_size_param(std::vector<std::string>* extra_params_vec,
     }
     return 0;
 }
+#endif
 
 static int create_crypto_blk_dev(struct crypt_mnt_ftr* crypt_ftr, const unsigned char* master_key,
                                  const char* real_blk_name, char* crypto_blk_name, const char* name,
